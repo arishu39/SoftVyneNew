235b66f2bb1d538fa45b8d9a7f366edb46488efe
Rishu <arishu39@yahoo.com>
2025-03-31 18:15:31 +0530

    data service change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './modules/shared/shared.module';
 import { AppComponent } from './app.component';
-import { DataService } from './services/data.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, SharedModule],
-  providers: [DataService],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
