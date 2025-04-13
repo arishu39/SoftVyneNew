5cc4e7f97f8f34e773ca451f924138ff0e6ac2d4
Rishu <arishu39@yahoo.com>
2025-04-13 15:14:39 +0530

    Design changes

--- src/app/modules/main/components/client/client.component.ts
+++ src/app/modules/main/components/client/client.component.ts
@@ -6,4 +6,19 @@ import { Component } from '@angular/core';
   styleUrl: './client.component.scss',
   standalone: false,
 })
-export class ClientComponent {}
+export class ClientComponent {
+  logos = [
+    { src: 'assets/part/client_1_120_60.png', alt: 'Client 1' },
+    { src: 'assets/part/client_2_120_60.png', alt: 'Client 2' },
+    { src: 'assets/part/client_3_90_60.png', alt: 'Client 3' },
+    { src: 'assets/part/client_4_120_60.png', alt: 'Client 4' },
+    { src: 'assets/part/client_5_120_60.png', alt: 'Client 5' },
+    { src: 'assets/part/client_6_120_60.png', alt: 'Client 6' },
+    { src: 'assets/part/client_7_120_60.png', alt: 'Client 7' },
+    { src: 'assets/part/client_8_120_60.png', alt: 'Client 8' },
+    { src: 'assets/part/client_9_120_60.png', alt: 'Client 9' },
+    { src: 'assets/part/client_10_120_60.png', alt: 'Client 10' },
+    { src: 'assets/part/client_11_120_60.png', alt: 'Client 11' },
+    { src: 'assets/part/client_12_120_60.png', alt: 'Client 12' },
+  ];
+}
